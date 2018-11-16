020aec60a8ba66abbb037cbfd62d34fa08b38225
susird <susir95@gmail.com>
2018-11-15 20:08:06 -0500

    post controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,22 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :content, presence: true
+
+  scope :only_published, -> { where(published: true) }
+  scope :popular, -> { where("views > ?", 10)}
+  # Ex:- scope :active, -> {where(:active => true)}
+  # Ex:- scope :active, -> {where(:active => true)}
+
+ # :phone
+ #validates :phone, numericality: {only_integer: true}
+
+#:points
+#validates :points, numericality: {greater_than: 5}
+#validates :points, numericality: {greater_than_or_equal_to: 5}
+#validates :points, numericality: {equal_to: 5}
+#validates :points, numericality: {less_than: 5}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,25 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index' 
 
+  resources :posts  
+
+  
+
   #Home rute
-  get 'home/index',      to: 'home#index'
-  get 'home/about',      to: 'home#about'
-  get 'home/contact_us', to: 'home#contact_us'
-  get 'home/prices',     to: 'home#prices'
+  # get 'home/index',      to: 'home#index'
+  # get 'home/about',      to: 'home#about'
+  # get 'home/contact_us', to: 'home#contact_us'
+  # get 'home/prices',     to: 'home#prices'
 
-  #blog routes
-  get 'blog/index'
-  get 'blog/new'
-  get 'blog/show'
-  get 'blog/edit'
+  # #blog routes
+  # get 'blog/index'
+  # get 'blog/new'
+  # get 'blog/show'
+  # get 'blog/edit'
 
-  #admin routs
-  get 'admin/index'
-  get 'admin/show'
+  # #admin routs
+  # get 'admin/index'
+  # get 'admin/show'
   
   
   
