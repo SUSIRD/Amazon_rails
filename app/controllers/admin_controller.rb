a96d514edcc0c0db14fd69cc9f96202c32eccb4b
susird <susir95@gmail.com>
2018-12-07 20:36:30 -0500

    Admin tables

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,22 @@
 class AdminController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_admin?
+
+  layout "admin"
+
   def index
+    @users = User.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc)
+    @comments = Comment.all.count
+    @subscriptors = Subscriptor.all.order(created_at: :desc)
   end
 
+  private
+  def is_admin?
+    unless current_user.admin?
+      flash[:alert] ="You don't have permissions"
+      redirect_to root_path
+    end
+  end
   
 end
