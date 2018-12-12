478aa7431fbe521a3118146fdb9da0075105dcbe
susird <susir95@gmail.com>
2018-12-11 19:22:51 -0500

    Button alert

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  name       :string
+#
+
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
